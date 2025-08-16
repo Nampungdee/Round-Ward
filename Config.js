ab1305d65cc8d6383faad88b9b08e6148518ea77
น้ำผึ้ง <Nampungdee5555@gmail.com>
2025-08-16 22:58:19 +0700

    Delete Config.js

--- Config.js
+++
@@ -1,8 +0,0 @@
-
-// ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbytzCc14xlv33J0CGawNKzqodiSznhdiyL1AhAow7gE3iI9U56fFt-0SdKkEItRYmI/exec';
-    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbz7jTqjdKWv6TJxeltf4ZDq4Y7Btc6gICuhmOoy9a0wUhCYZpaPVG2CMCJkY-1bHi65/exec';
-//LIFF_ID
-    const LIFF_ID = '2007868023-VD36zPx6'; // LIFF ID Checkin
-    const LIFF_HISTORY = '2007868023-z4ZY4g9Y'; // LIFF ID history
-    const LIFF_REGISTER = '2007868023-QvDrAKlr'; // LIFF ID register
