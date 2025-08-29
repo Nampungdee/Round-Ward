0d26166186fc653b5f02c31b26b6512db2311e32
น้ำผึ้ง <Nampungdee5555@gmail.com>
2025-08-29 15:43:00 +0700

    Update Config.js
    update line liff new

--- Config.js
+++ Config.js
@@ -2,8 +2,9 @@
     const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbxXV5_BlLVtGPr-SWuCmFHn6M29xtWxhBQQ2ggXNGkENKO0HABo9OSQtCCdO36s03Ys/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwjH2KB5-IxqxTPVjhQUcnuj2PCF9zexoRLqk-ERC1o4FxyLhk6YfJN4z5pcN1T4OQa/exec';
 //LIFF_ID
-    const LIFF_ID = '2007747842-OGeyrGkv'; // LIFF ID Checkin
-    const LIFF_HISTORY = '2007747842-p0YX1oBn'; // LIFF ID history
-    const LIFF_REGISTER = '2007747842-oJr1GXK2'; // LIFF ID register
+    const LIFF_ID = '2007868023-VD36zPx6'; // LIFF ID Checkin
+    const LIFF_HISTORY = '2007868023-z4ZY4g9Y'; // LIFF ID history
+    const LIFF_REGISTER = '2007868023-QvDrAKlr'; // LIFF ID register
+
 
 
